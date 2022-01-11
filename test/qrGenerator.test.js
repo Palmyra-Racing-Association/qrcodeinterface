c1556d3045bf4197df0bc21f4faa9cb6f354558d
Alan Delimon <alan.delimon@skillsoft.com>
2022-01-10 23:16:42 -0500

    Added example and test directory missed in last commit.

---
+++ test/qrGenerator.test.js
@@ -0,0 +1,30 @@
+const QRCodeInterface = require('../qrCodeInterface');
+
+test('encode and decode data match', async() => {
+  const initial = '47-14580-2022';
+  const generator = new QRCodeInterface();
+  const qrImg = await generator.getQrDataUrl(initial);
+  const decoded = await generator.decode(qrImg);
+  console.log(decoded);
+  expect(decoded).toBe(initial);
+});
+
+test('encode throws an error', async() => {
+  const generator = new QRCodeInterface();
+  try {
+    const qrImg = await generator.getQrDataUrl('');    
+  } catch (error) {
+    expect(error).toBeDefined();
+  }
+
+});
+
+
+test('decode throws an error', async() => {
+  const generator = new QRCodeInterface();
+  try {
+    const decoded = await generator.decode('');  
+  } catch (error) {
+    expect(error).toBeDefined();
+  }
+});
